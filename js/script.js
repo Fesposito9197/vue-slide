328860def2198817266e8a6fd9d216a92caf395a
Fesposito9197 <kekkoesp8@gmail.com>
2022-11-15 12:31:13 +0100

    correzione 3 bonus

--- js/script.js
+++ js/script.js
@@ -4,25 +4,29 @@ const { createApp } = Vue
     data() {
       return {
         tabindex: 0 ,
-        
+        autoplay: null,
         slides: [
             {
                 image: 'img/01.webp',
                 title: "Marvel's Spiderman Miles Morale",
                 text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-            }, {
+            }, 
+            {
                 image: 'img/02.webp',
                 title: 'Ratchet & Clank: Rift Apart',
                 text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-            }, {
+            }, 
+            {
                 image: 'img/03.webp',
                 title: 'Fortnite',
                 text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-            }, {
+            }, 
+            {
                 image: 'img/04.webp',
                 title: 'Stray',
                 text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-            }, {
+            }, 
+            {
                 image: 'img/05.webp',
                 title: "Marvel's Avengers",
                 text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
@@ -33,7 +37,7 @@ const { createApp } = Vue
     methods:{
         prevImage (){
             
-            if (this.tabindex < 0) {
+            if (this.tabindex === 0) {
                 this.tabindex = this.slides.length - 1
             }else{
                 this.tabindex--
@@ -41,19 +45,26 @@ const { createApp } = Vue
         },
         nextImage () {
             
-            if (this.tabindex > this.slides.length - 1) {
+            if (this.tabindex === this.slides.length - 1) {
                 this.tabindex = 0
             }else{
                 this.tabindex++
             }
         },
-        changeImage (index){
-            this.tabindex = index
+        changeImage (i){
+            this.tabindex = i
         },
-      
+        
+        mouseleave(){
+            this.autoplay = setInterval(this.nextImage,3000)
+        },
+        mouseover (){
+            clearInterval(this.autoplay)
+        }
+        
     },
     mounted () {
-       setInterval(this.nextImage , 3000)
+        this.mouseleave()
     } 
   }).mount('#app')
 
